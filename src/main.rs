89f4dbe0090af711656b38bde0c540bf89e94f55
Ryan Summers <ryan.summers@vertigo-designs.com>
2020-07-03 16:25:37 +0200

    Fixing format

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,7 @@
 #![no_std]
 #![no_main]
 
-// pick a panicking behavior
-use panic_halt as _; // you can put a breakpoint on `rust_begin_unwind` to catch panics
-// use panic_abort as _; // requires nightly
-// use panic_itm as _; // logs messages over ITM; requires ITM support
-// use panic_semihosting as _; // logs messages to the host stderr; requires a debugger
+use panic_halt as _;
 
 use cortex_m::asm;
 use cortex_m_rt::entry;
