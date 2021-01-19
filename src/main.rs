e845f566af4bf556df0f92a2229334ff70f267df
occheung <dc@m-labs.hk>
2021-01-19 13:14:01 +0800

    nal: implement enc424j600 variant

--- src/main.rs
+++ src/main.rs
@@ -52,6 +52,8 @@ use logger::BufferedLog;
 use rf_channel::{AdcPin, AnalogPins as AdcPins, ChannelPins as RfChannelPins, ChannelState};
 use serial_terminal::SerialTerminal;
 use settings::BoosterSettings;
+#[cfg(feature = "phy_enc424j600")]
+use smoltcp_nal::NetworkStack;
 use user_interface::{ButtonEvent, Color, UserButtons, UserLeds};
 use watchdog::{WatchdogClient, WatchdogManager};
 
@@ -86,6 +88,9 @@ type SPI = hal::spi::Spi<
 #[cfg(feature = "phy_w5500")]
 type Ethernet =
     w5500::Interface<hal::gpio::gpioa::PA4<hal::gpio::Output<hal::gpio::PushPull>>, SPI>;
+#[cfg(feature = "phy_enc424j600")]
+type Ethernet = NetworkStack<'static, 'static, 'static>;
+
 type MqttClient = minimq::MqttClient<minimq::consts::U1024, Ethernet>;
 
 type I2cBusManager = mutex::AtomicCheckManager<I2C>;
@@ -158,6 +163,38 @@ pub struct MainBus {
     pub channels: BoosterChannels,
     pub fans: ChassisFans,
 }
+
+/// Container for smoltcp-related network configurations
+#[cfg(feature = "phy_enc424j600")]
+pub struct NetStorage {
+    pub socket_storage: [Option<smoltcp::socket::SocketSetItem<'static, 'static>>; 1],
+    pub ip_addrs: [smoltcp::wire::IpCidr; 1],
+    pub neighbor_cache: [Option<(smoltcp::wire::IpAddress, smoltcp::iface::Neighbor)>; 8],
+    pub routes_cache: [Option<(smoltcp::wire::IpCidr, smoltcp::iface::Route)>; 8],
+}
+
+#[cfg(feature = "phy_enc424j600")]
+pub struct DataStorage {
+    pub tx_storage: [u8; 4096],
+    pub rx_storage: [u8; 1024],
+}
+
+#[cfg(feature = "phy_enc424j600")]
+static mut NET_STORE: NetStorage = NetStorage {
+    // Placeholder for the real IP address, which is initialized at runtime.
+    ip_addrs: [smoltcp::wire::IpCidr::Ipv6(
+        smoltcp::wire::Ipv6Cidr::SOLICITED_NODE_PREFIX,
+    )],
+    neighbor_cache: [None; 8],
+    routes_cache: [None; 8],
+    socket_storage: [None; 1],
+};
+
+#[cfg(feature = "phy_enc424j600")]
+static mut DATA_STORE: DataStorage = DataStorage {
+    tx_storage: [0; 4096],
+    rx_storage: [0; 1024],
+};
 
 #[rtic::app(device = stm32f4xx_hal::stm32, peripherals = true, monotonic = rtic::cyccnt::CYCCNT)]
 const APP: () = {
@@ -396,24 +433,102 @@ const APP: () = {
 
                 #[cfg(feature = "phy_w5500")]
                 {
-                let mut w5500 = w5500::W5500::new(
-                    spi,
-                    cs,
-                    w5500::OnWakeOnLan::Ignore,
-                    w5500::OnPingRequest::Respond,
-                    w5500::ConnectionType::Ethernet,
-                    w5500::ArpResponses::Cache,
-                )
-                .unwrap();
+                    let mut w5500 = w5500::W5500::new(
+                        spi,
+                        cs,
+                        w5500::OnWakeOnLan::Ignore,
+                        w5500::OnPingRequest::Respond,
+                        w5500::ConnectionType::Ethernet,
+                        w5500::ArpResponses::Cache,
+                    )
+                    .unwrap();
+    
+                    w5500.set_mac(settings.mac()).unwrap();
+    
+                    // Set default netmask and gateway.
+                    w5500.set_gateway(settings.gateway()).unwrap();
+                    w5500.set_subnet(settings.subnet()).unwrap();
+                    w5500.set_ip(settings.ip()).unwrap();
+    
+                    w5500::Interface::new(w5500)
+                }
 
-                w5500.set_mac(settings.mac()).unwrap();
+                #[cfg(feature = "phy_enc424j600")]
+                {
+                    use smoltcp as net;
+                    use enc424j600::EthController;
 
-                // Set default netmask and gateway.
-                w5500.set_gateway(settings.gateway()).unwrap();
-                w5500.set_subnet(settings.subnet()).unwrap();
-                w5500.set_ip(settings.ip()).unwrap();
+                    let mut enc424j600 = enc424j600::SpiEth::new(spi, cs);
 
-                w5500::Interface::new(w5500)
+                    match enc424j600.init_dev(&mut delay) {
+                        Ok(_) => {},
+                        Err(_) => {
+                            panic!("ENC424J600 PHY initialization failed");
+                        },
+                    }
+
+                    let mut eth_mac_addr: [u8; 6] = [0; 6];
+                    enc424j600.read_from_mac(&mut eth_mac_addr).unwrap();
+
+                    // Init Rx/Tx buffers
+                    enc424j600.init_rxbuf().unwrap();
+                    enc424j600.init_txbuf().unwrap();
+
+                    let eth_iface = unsafe {
+                        let device = enc424j600::smoltcp_phy::SmoltcpDevice::new(enc424j600);
+                        
+                        // TODO: Restore IP config from EEPROM
+                        NET_STORE.ip_addrs[0] = {
+                            let ip = settings.ip().octets();
+                            let subnet = settings.subnet().octets();
+                            net::wire::IpCidr::new(
+                                net::wire::IpAddress::from(
+                                    net::wire::Ipv4Address::from_bytes(&ip)
+                                ),
+                                net::wire::IpAddress::from(
+                                    net::wire::Ipv4Address::from_bytes(&subnet)
+                                ).to_prefix_len().unwrap()
+                            )
+                        };
+
+                        let routes = {
+                            // TODO: Restore gateway config from EEPROM
+                            let gateway = net::wire::Ipv4Address::from_bytes(
+                                &settings.gateway().octets()
+                            );
+
+                            let mut routes = net::iface::Routes::new(&mut NET_STORE.routes_cache[..]);
+                            routes.add_default_ipv4_route(gateway).unwrap();
+                            routes
+                        };
+
+                        let neighbor_cache = net::iface::NeighborCache::new(&mut NET_STORE.neighbor_cache[..]);
+
+                        net::iface::EthernetInterfaceBuilder::new(device)
+                            .ethernet_addr(net::wire::EthernetAddress(eth_mac_addr))
+                            .neighbor_cache(neighbor_cache)
+                            .ip_addrs(&mut NET_STORE.ip_addrs[..])
+                            .routes(routes)
+                            .finalize()
+
+                    };
+
+                    let socket_set = unsafe {
+                        let mut sockets = net::socket::SocketSet::new(&mut NET_STORE.socket_storage[..]);
+                
+                        let mut tcp_socket = {
+                            let tx_buffer = net::socket::TcpSocketBuffer::new(&mut DATA_STORE.tx_storage[..]);
+                            let rx_buffer = net::socket::TcpSocketBuffer::new(&mut DATA_STORE.rx_storage[..]);
+                
+                            net::socket::TcpSocket::new(rx_buffer, tx_buffer)
+                        };
+                        tcp_socket.set_keep_alive(Some(net::time::Duration::from_millis(1000)));
+
+                        let _handle = sockets.add(tcp_socket);
+                        sockets
+                    };
+
+                    NetworkStack::new(eth_iface, socket_set)
                 }
             };
 
