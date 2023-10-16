bddcc3f54dff807cab05bdfd22293cb2fd640c5d
Ryan Summers <ryan.summers@vertigo-designs.com>
2023-10-16 11:41:01 +0200

    Fixing build.rs file

--- build.rs
+++ build.rs
@@ -3,13 +3,9 @@
 //! # Note
 //! This build script is run immediately before the build is completed and is used to inject
 //! environment variables into the build.
-use std::env;
-
 fn main() {
-    let mut options = built::Options::default();
-    options.set_git(true);
-    let src = env::var("CARGO_MANIFEST_DIR").unwrap();
-    let dst = std::path::Path::new(&env::var("OUT_DIR").unwrap()).join("built.rs");
-    built::write_built_file_with_opts(&options, src.as_ref(), &dst)
-        .expect("Failed to acquire build-time information");
+    //for (k, v) in std::env::vars_os() {
+    //    println!("{}: {}", k.into_string().unwrap(), v.into_string().unwrap());
+    //}
+    built::write_built_file().expect("Failed to acquire build-time information");
 }
