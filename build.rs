54458b688007afef8e5c0c927d5e15b6ee082dca
Ryan Summers <ryan.summers@vertigo-designs.com>
2023-10-16 11:49:52 +0200

    Fixing build file

--- build.rs
+++ build.rs
@@ -4,8 +4,5 @@
 //! This build script is run immediately before the build is completed and is used to inject
 //! environment variables into the build.
 fn main() {
-    //for (k, v) in std::env::vars_os() {
-    //    println!("{}: {}", k.into_string().unwrap(), v.into_string().unwrap());
-    //}
     built::write_built_file().expect("Failed to acquire build-time information");
 }
