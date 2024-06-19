7b1d043e4a2db2acb9c76f7e409da418c4798d21
Ryan Summers <ryan.summers@vertigo-designs.com>
2024-06-19 18:55:40 +0200

    Fixing clippy

--- src/settings/eeprom/mod.rs
+++ src/settings/eeprom/mod.rs
@@ -8,7 +8,7 @@ use serde::{Deserialize, Serialize};
 
 /// A semantic version control for recording software versions.
 #[derive(
-    Encode, DecodeOwned, Serialize, Deserialize, Debug, PartialEq, PartialOrd, Eq, Copy, Clone,
+    Encode, DecodeOwned, Serialize, Deserialize, Debug, PartialEq, Eq, Copy, Clone,
 )]
 pub struct SemVersion {
     major: u8,
@@ -20,6 +20,12 @@ impl SemVersion {
     /// Determine if this version is compatible with `rhs`.
     pub fn is_compatible_with(&self, rhs: &SemVersion) -> bool {
         self.major == rhs.major
+    }
+}
+
+impl core::cmp::PartialOrd for SemVersion {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
     }
 }
 
