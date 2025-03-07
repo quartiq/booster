892dc0a6fe658138b4bd7be20166cd681e50c426
Markus Reiter <me@reitermark.us>
2025-03-07 16:58:34 +0100

    Silently clamp `output_interlock_threshold`.

--- src/settings/eeprom/rf_channel.rs
+++ src/settings/eeprom/rf_channel.rs
@@ -168,10 +168,9 @@ impl ChannelSettings {
 
     fn validate_output_interlock_threshold(&mut self, depth: usize) -> Result<usize, &'static str> {
         // Verify the output interlock is within acceptable values.
-        if *self.output_interlock_threshold > platform::MAX_OUTPUT_POWER_DBM {
-            *self.output_interlock_threshold = platform::MAX_OUTPUT_POWER_DBM;
-            return Err("Output interlock threshold too high");
-        }
+        *self.output_interlock_threshold = self
+            .output_interlock_threshold
+            .clamp(0.0, platform::MAX_OUTPUT_POWER_DBM);
 
         // Verify the interlock is mappable to a DAC threshold.
         let dac_voltage = self
@@ -180,7 +179,6 @@ impl ChannelSettings {
         let dac_voltage_clamped = dac_voltage.clamp(0.0, ad5627::MAX_VOLTAGE);
         if dac_voltage_clamped != dac_voltage {
             *self.output_interlock_threshold = self.output_power_transform.map(dac_voltage_clamped);
-            return Err("Output interlock threshold voltage out of range");
         }
 
         Ok(depth)
